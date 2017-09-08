0844d4e28ef5f0cd76a342ccab404876c1d977a2
Zhluktenko Dmytro <d1mnewz@gmail.com>
2017-09-08 13:37:13 +0300

    demo for C# 7.1

--- 01_Demo/Animal.cs
+++
@@ -1,7 +0,0 @@
-namespace _01_Demo
-{
-    internal abstract class Animal
-    {
-        public abstract (int ID, string AnimalName) M1();
-    }
-}
--- 01_Demo/Program.cs
+++ 01_Demo/Program.cs
@@ -1,6 +1,49 @@
-﻿namespace _01_Demo
-{
-	// watch some videos from TODO and implement in demo. 
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
 
-	// Changing element names when overriding could be confusing to the caller.
+namespace _01_Demo
+{
+	internal class Program
+	{
+		public static async Task Main()
+		{
+			WriteLine("hello");
+			object[] arr = { "Dmytro", -42, "42", 420, new LockCookie(), new object[] { -420, 23, 17 } };
+			await PrintTypes(arr);
+
+		}
+
+		private static async Task PrintTypes(object[] arr,
+			CancellationToken cancellationToken = default,
+			DateTimeOffset calculationDate = default)
+		{
+			foreach (var obj in arr)
+			{
+				switch (obj)
+				{
+					case int i when i < 0:
+						WriteLine($"{i} is {i.GetType().Name} < 0");
+						break;
+					case int i when i >= 0:
+						WriteLine($"{i} is {i.GetType().Name} >= 0");
+						break;
+					case string str when Int32.TryParse(str, out _):
+						WriteLine($"{str} is {str.GetType().Name}, but may be parsed as int");
+						break;
+					case string str when !Int32.TryParse(str, out _):
+						WriteLine($"{str} is {str.GetType().Name} and can't be parsed as int");
+						break;
+					case object[] objArr when objArr.Length > 0:
+						await PrintTypes(objArr, cancellationToken);
+						break;
+					case null:
+						break;
+					default:
+						throw new ArgumentException("Invalid parameter", nameof(obj));
+				}
+			}
+		}
+	}
 }
