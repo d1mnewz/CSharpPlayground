48b2f0de32180a585da1216e81a07194ec86d1c1
Zhluktenko Dmytro <d1mnewz@gmail.com>
2017-10-11 00:51:10 +0300

    added concurrency samples

---
+++ 04_ApiConcurrencyTest/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace _04_ApiConcurrencyTest.Controllers
+{
+	public class ValuesController : ApiController
+	{
+		// GET api/sync
+		[Route("api/sync")]
+		public void Get() => Thread.Sleep(TimeSpan.FromSeconds(5));
+
+		// GET api/async
+		[Route("api/async")]
+		public async Task GetAsync() => await Task.Delay(TimeSpan.FromSeconds(5));
+	}
+}
---
+++ 05_ApiConcurrencyTestCore/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05_ApiConcurrencyTestCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+	    // GET api/sync
+	    [Route("api/sync")]
+	    public void Get() => Thread.Sleep(TimeSpan.FromSeconds(5));
+
+	    // GET api/async
+	    [Route("api/async")]
+	    public async Task GetAsync() => await Task.Delay(TimeSpan.FromSeconds(5));
+	}
+}
