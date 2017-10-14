d161cacabccb5a3da774dadbed12a1154a1b2858
Zhluktenko Dmytro <d1mnewz@gmail.com>
2017-10-14 18:30:57 +0300

    quick code refactoring

---
+++ 08_DictionaryTryGetContains/DictionaryPerformanceRunner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace _08_DictionaryTryGetContains
+{
+	public class DictionaryPerformanceRunner : IPerformanceRunner
+	{
+		private const int Size = 1000000;
+
+		private readonly Dictionary<int, string> _dictionary;
+
+		public DictionaryPerformanceRunner()
+		{
+			_dictionary = new Dictionary<int, string>();
+
+			for (var i = 0; i < Size; i++)
+			{
+				_dictionary.Add(i, i.ToString());
+			}
+		}
+
+
+		public string UsingContainsKey()
+		{
+			var result = string.Empty;
+			for (var i = 0; i < Size; i++)
+			{
+				if (_dictionary.ContainsKey(i))
+				{
+					result = _dictionary[i];
+				}
+			}
+			return result;
+		}
+
+		public string UsingTryGetValue()
+		{
+			var result = string.Empty;
+			for (var i = 0; i < Size; i++)
+			{
+				_dictionary.TryGetValue(i, out result);
+			}
+			return result;
+		}
+	}
+
+	public interface IPerformanceRunner
+	{
+	}
+}
--- 08_DictionaryTryGetContains/Program.cs
+++ 08_DictionaryTryGetContains/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace _08_DictionaryTryGetContains
@@ -32,46 +31,5 @@ namespace _08_DictionaryTryGetContains
 			Console.WriteLine($"TryGetValue is { 100 - Math.Round(tryget / (double)contains * 100, 2)} % faster than Contains");
 		}
 
-	}
-
-	public class DictionaryPerformanceRunner
-	{
-		private const int Size = 1000000;
-
-		private readonly Dictionary<int, string> _dictionary;
-
-		public DictionaryPerformanceRunner()
-		{
-			_dictionary = new Dictionary<int, string>();
-
-			for (var i = 0; i < Size; i++)
-			{
-				_dictionary.Add(i, i.ToString());
-			}
-		}
-
-
-		public string UsingContainsKey()
-		{
-			var result = string.Empty;
-			for (var i = 0; i < Size; i++)
-			{
-				if (_dictionary.ContainsKey(i))
-				{
-					result = _dictionary[i];
-				}
-			}
-			return result;
-		}
-
-		public string UsingTryGetValue()
-		{
-			var result = string.Empty;
-			for (var i = 0; i < Size; i++)
-			{
-				_dictionary.TryGetValue(i, out result);
-			}
-			return result;
-		}
 	}
 }
