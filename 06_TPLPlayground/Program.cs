09f8293e8eb1336da49f42161555975dfc6f61e2
Zhluktenko Dmytro <d1mnewz@gmail.com>
2017-10-14 17:32:06 +0300

    some mutithreading child play

---
+++ 06_TPLPlayground/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _06_TPLPlayground
+{
+	class Program
+	{
+		public static async Task Main()
+		{
+			Foo f = new Foo();
+			for (int i = 0; i < 100; i++)
+			{
+				await f.SomeOperationAsync();
+			}
+
+		}
+		~Program()
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
+	public class Foo
+	{
+		private void SomeOperation()
+		{
+			Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+		}
+
+		public async Task SomeOperationAsync()
+		{
+			Task t = new Task(SomeOperation);
+			t.Start();
+			await t;
+		}
+
+	}
+}
+
---
+++ 07_ThreadPool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace _07_ThreadPool
+{
+    class Program
+    {
+        static void Main()
+        {
+	        var logicalProcessorCount = System.Environment.ProcessorCount;
+	        ThreadPool.GetMinThreads(out var minimumWorkerThreadCount, out var minimumIocThreadCount);
+	        Console.WriteLine($"No.of processors: {logicalProcessorCount}");
+	        Console.WriteLine($"Minimum no.of Worker threads: {minimumWorkerThreadCount}");
+	        Console.WriteLine($"Minimum no.of IOCP threads: {minimumIocThreadCount}");
+	        Console.Read();
+		}
+    }
+}
