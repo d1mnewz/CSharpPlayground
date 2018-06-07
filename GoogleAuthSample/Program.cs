1361fcffb3131a518fa779ec058d24ada7369c04
Dmytro Zhluktenko <d1mnewz@gmail.com>
2018-06-07 12:04:25 +0300

    Added some demos

---
+++ GoogleAuthSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Oauth2.v2;
+using Google.Apis.Services;
+
+namespace GoogleAuthSample
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			var accessToken =
+				"ya29.GlvTBZnj-NGxW28OIE4jOjEsIQJh2XgxSsuDioj3VapUYeiT_D7TT6zwePEbOhLQ_dO0QlkRh5l_WX7fWm7oTnS75RWEk5OGN45tvheYwWyt0v0jexUSTO0bbETF";
+			var credentials = GoogleCredential.FromAccessToken(
+				accessToken);
+			var service = new Oauth2Service(new BaseClientService.Initializer
+			{
+//				ApplicationName = "Maneno",
+//				ApiKey = "46114941870-tur24h3f1f0ib16ap9gqvaoj879il06l.apps.googleusercontent.com",
+				HttpClientInitializer = credentials
+			});
+			var user = service.Userinfo.V2.Me.Get().Execute();
+//			return Task.FromResult(new User
+//			{
+//				Email = user.Email,
+//				FirstName = user.GivenName,
+//				LastName = user.FamilyName,
+//				Name = user.Name,
+//				UserName = user.Email
+//			});
+		}
+	}
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace LinkedList
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello World!");
+			var list = new LinkedList<int>();
+			list.AddLast(0);
+			list.AddLast(3);
+			list.AddFirst(5);
+			Console.WriteLine(list.ToString());
+			Console.ReadKey();
+		}
+	}
+
+	public class Node<T>
+	{
+		public T Value { get; set; }
+		public Node<T> Next { get; set; }
+	}
+
+	public class LinkedList<T>
+	{
+		private Node<T> _head;
+		private Node<T> _tail;
+
+		public void AddLast(T value)
+		{
+			if (_head is null)
+			{
+				_head = new Node<T> { Value = value };
+				_tail = _head;
+			}
+			else
+			{
+				_tail.Next = new Node<T> { Value = value };
+				_tail = _tail.Next;
+			}
+		}
+
+		public void AddFirst(T value)
+		{
+			var newHead = new Node<T> { Value = value, Next = _head };
+			_head = newHead;
+		}
+
+		public override string ToString()
+		{
+			if (_head != null)
+			{
+				var sb = new StringBuilder();
+				var current = _head;
+				sb.Append($"{current.Value} ");
+
+				while (current.Next != null)
+				{
+					current = current.Next;
+					sb.Append($"{current.Value} ");
+				}
+
+
+				return sb.ToString();
+			}
+
+			return String.Empty;
+		}
+	}
+}
